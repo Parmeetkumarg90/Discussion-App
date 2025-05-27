0422aa65796d9df815cd63c5dc4eb07bd2cf93bb
Parmeet Kumar <parmeetkumarg90@gmail.com>
2025-05-27 13:25:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // Local storage - 👍
-// sorting in responses according to like - dislike diff - 👍
+// sorting in responses according to (like - dislike diff) optimize way - 👍
 // search and highlight - 👍
 // event delegation - 👍
 // favourite marked - 👍
 // make less refresh or use of DOM in submit button, search button, favourite button, resolve button - 👍
 // use id not values 51,107,283,293,305 - 👍
-// add timer to each question
+// add timer to each question - 👍
 
-let AllData, questionFlag = false, favFlag = false, imgSrc = ["not favourite.png", "favourite.png"];
+let AllData, questionFlag = false, favFlag = false, timerFlag, imgSrc = ["not favourite.png", "favourite.png"];
 AllData = JSON.parse(localStorage.getItem('AllData')) || [];
 
 if (AllData != []) {
     showQuestion();
+    timer();
 }
 
 document.querySelector('#newFormButton').addEventListener('click', (event) => { // new form button
@@ -40,19 +41,21 @@ document.querySelector('.searchField').addEventListener('input', (event) => { //
 document.querySelector('#resolveButton').addEventListener('click', (event) => {  // resolve submit button
     event.stopPropagation();
     const ques = document.querySelector('.displayQues');
-    // if(!span) return;
-    const spanID = ques.getAttribute('data-id');
+    if (!ques) return;
+    const spanID = Number(ques.getAttribute('data-id'));
     resolve(spanID);
 });
 
 document.querySelector('#submitButton').addEventListener('click', (event) => {   // add response
     event.stopPropagation();
     storeResponse();
+    // popularResp(index); // sort the responses
 });
 
 document.querySelector('.allResponse').addEventListener('click', (event) => {   // like or dislike
     event.stopPropagation();
     let ques = document.querySelector('.displayQues');
+    if (!ques) return;
     let spanID = Number(ques.getAttribute('data-id'));
     // console.log(ques);
 
@@ -66,9 +69,6 @@ document.querySelector('.allResponse').addEventListener('click', (event) => {   
             // console.log("question index = ",resIndex)
         }
     });
-
-    // if(!resIndex) return;
-
     if (event.target.id === 'like') {
         incLike(resIndex, event.target.closest('span'));
     }
@@ -81,9 +81,8 @@ document.querySelector('.allResponse').addEventListener('click', (event) => {   
 document.querySelector('.body').addEventListener('click', (event) => { // any question click in question box
     event.stopPropagation();
     let span = event.target.closest("span");   // finding which question is clicked by event delegation
-    if (span === null) {
-        return;
-    }
+    if (!span) return;
+
     let spanID = Number(span.getAttribute('data-id'));
     // let h1 = span.querySelector('h1').innerText;
     // let p = span.querySelector('p').innerText;
@@ -102,7 +101,7 @@ document.querySelector('.body').addEventListener('click', (event) => { // any qu
     AllData.find((item, index) => {
         if (item.id === spanID) {
             showResponse(index, spanID);
-            return;
+            // popularResp(index); // sort the responses
         }
     });
 });
@@ -116,9 +115,10 @@ function showQuestion() {  // append all question
 function storeResponse() { // store response in array
     let name = document.querySelector('.userName').value;
     let response = document.querySelector('.userResponse').value;
-
     if (name != "" && response != "") {
         let displayQues = document.querySelector('.displayQues');
+        if (!displayQues) return;
+
         let spanID = Number(displayQues.getAttribute('data-id'));
         let obj = {
             id: Date.now(),
@@ -133,8 +133,9 @@ function storeResponse() { // store response in array
                 item.response.push(obj);
             }
         });
+        localStorage.setItem('AllData', JSON.stringify(AllData));
+        clearInterval(timerFlag);
         addResponse(obj);
-
         document.querySelector('.userResponse').value = "";
         document.querySelector('.userName').value = "";
     }
@@ -171,12 +172,12 @@ function showResponse(index, spanID) { // display add responses
     document.querySelector('.ques').style.display = 'none';
     document.querySelector('.response').style.display = 'flex';
     let displayQues = document.querySelector('.displayQues');
+    if (!displayQues) return;
     displayQues.querySelector('h1').innerText = AllData[index].subject;
     displayQues.querySelector('p').innerText = AllData[index].question;
     displayQues.setAttribute('data-id', spanID);
-    let allResponse = document.querySelector('.allResponse');
-    allResponse.innerHTML = "";
-    popularResp(index); // sort the responses
+    document.querySelector('.allResponse').innerHTML = '';
+    // console.log("empty response");
     if (AllData[index].response.length > 0) {
         AllData[index].response.forEach((item) => {
             addResponse(item);
@@ -184,12 +185,30 @@ function showResponse(index, spanID) { // display add responses
     }
 }
 
-function popularResp(index) {
-    AllData[index].response = AllData[index].response.sort((a, b) => (b.like - b.dislike) - (a.like - a.dislike));
+function popularResp(quesIndex) {
+    AllData[quesIndex].response = AllData[quesIndex].response.sort((a, b) => (b.like - b.dislike) - (a.like - a.dislike));
+    let allResponse = document.querySelector('.allResponse').querySelectorAll('span');
+    allResponse.forEach((item, index) => {
+        shiftResponses(item, quesIndex, index);
+    });
+}
+
+function shiftResponses(span, quesIndex, index) {
+    span.setAttribute('data-id', AllData[quesIndex].response[index].id);
+
+    span.querySelector('h1').innerText = AllData[quesIndex].response[index].name;
+    span.querySelector('p').innerText = AllData[quesIndex].response[index].response;
+
+    span.childNodes[1].childNodes[1].nodeValue = AllData[quesIndex].response[index].like;
+    span.childNodes[1].childNodes[3].nodeValue = AllData[quesIndex].response[index].dislike;
+    // console.log(span.childNodes[1].childNodes[1]);
+    // console.log(span.childNodes[1].childNodes[3]);
 }
 
 function addResponse(obj) { // append new response
     let allResponse = document.querySelector('.allResponse');
+    if (!allResponse) return;
+
     let span = document.createElement('span');
     span.setAttribute('data-id', obj.id);
     span.style.width = '100%';
@@ -216,6 +235,7 @@ function addResponse(obj) { // append new response
     let like = document.createElement('div');
     let likeImg = document.createElement('img');
     const likeText = document.createTextNode(obj.like);
+
     likeImg.src = 'like.png';
     likeImg.style.width = '4vmax';
     likeImg.style.padding = '0.5vmax';
@@ -224,6 +244,7 @@ function addResponse(obj) { // append new response
     let dislike = document.createElement('div');
     let dislikeImg = document.createElement('img');
     const dislikeText = document.createTextNode(obj.dislike);
+
     dislikeImg.src = 'dislike.png';
     dislikeImg.style.width = '4vmax';
     dislikeImg.style.padding = '0.5vmax';
@@ -231,7 +252,6 @@ function addResponse(obj) { // append new response
 
     h1.innerText = obj.name;
     p.innerText = obj.response;
-
     h1.style.width = "100%";
     p.style.width = "100%";
 
@@ -250,6 +270,7 @@ function addResponse(obj) { // append new response
 
 function showFavQuestion() {
     let questions = document.querySelector('.body').querySelectorAll('span');
+    if (!questions) return;
     if (favFlag) {
         questions.forEach(item => {
             // console.log(item.querySelector('img').src.includes('not'));
@@ -273,13 +294,14 @@ function showFavQuestion() {
 
 function resolve(spanID) { // remove from array
     let questions = document.querySelector('.body').querySelectorAll('span');
+    if (!questions) return;
     questions.forEach(item => {
-        if (item.getAttribute('data-id') === spanID) {
+        if (Number(item.getAttribute('data-id')) === spanID) {
             item.remove();
         }
     });
-
     AllData = AllData.filter((item) => item.id !== spanID);
+    // console.log(AllData)
     localStorage.setItem('AllData', JSON.stringify(AllData));
     document.querySelector('.ques').style.display = 'none';
     document.querySelector('.response').style.display = 'none';
@@ -292,7 +314,8 @@ function incLike(index, span) {  // increase like
     AllData[index].response.forEach(item => {
         if (item.id === spanID) {
             item.like += 1;
-            showResponse(index, AllData[index].id);
+            // showResponse(index, AllData[index].id);
+            popularResp(index);
         }
     });
 }
@@ -303,7 +326,8 @@ function incDislike(index, span) { // increase dislike
     AllData[index].response.forEach(item => {
         if (item.id === spanID) {
             item.dislike += 1;
-            showResponse(index, AllData[index].id);
+            // showResponse(index, AllData[index].id);
+            popularResp(index);
         }
     });
 }
@@ -311,6 +335,7 @@ function incDislike(index, span) { // increase dislike
 function searchQuestion() { // search the questions
     const searchText = document.querySelector('.searchField').value;
     let questions = document.querySelector('.body').querySelectorAll('span');
+    if (!questions) return;
     let h1, p;
     questions.forEach((item) => {
         h1 = item.querySelector('h1').innerText;
@@ -331,19 +356,16 @@ function searchQuestion() { // search the questions
     });
 }
 
-
 function addQuestion(item) { // append new question
     let quesBody = document.querySelector('.body');
+    if (!quesBody) return;
     let span = document.createElement('span');
     let h1 = document.createElement('h1');
     let p = document.createElement('p');
     let img = document.createElement('img');
-
-    //day 2
     let time = document.createElement('p');
     time.style.fontSize = "1.4vmax";
     time.style.width = '15vmax';
-
     h1.innerText = item.subject;
     p.innerText = item.question;
     img.src = imgSrc[item.favourite ? 1 : 0];
@@ -355,25 +377,32 @@ function addQuestion(item) { // append new question
     span.append(img);
     span.append(time); // day2
     quesBody.append(span);
-    setInterval(() => {
-        let allTime = getTime(item.id);
-        let result = 0;
-        // if (allTime.year) result = `${allTime.year} year`;
-        // else if (allTime.month) result = `${allTime.month} month`;
-        // else if (allTime.week) result = `${allTime.week} week`;
-        // else if (allTime.day) result = `${allTime.day} day`;
-        if (allTime.hour) {
-            result = `${('0' + allTime.hour).slice(-2)} hh ${('0' + allTime.minute).slice(-2)} min`;
-        } else if (allTime.minute) {
-            result = `${('0' + allTime.minute).slice(-2)} min ${('0' + allTime.second).slice(-2)} sec`;
-        } else if (allTime.second) {
-            result = `${('0' + allTime.second).slice(-2)} sec`;
-        }
+    timer();
+}
 
-        time.innerText = `${result}'s ago`;
+function timer() {
+    let span = document.querySelector('.body').querySelectorAll('span');
+    timerFlag = setInterval(() => {
+        span.forEach((item) => {
+            let allTime = getTime(Number(item.getAttribute('data-id')));
+            let result = 0;
+            // if (allTime.year) result = `${allTime.year} year`;
+            // else if (allTime.month) result = `${allTime.month} month`;
+            // else if (allTime.week) result = `${allTime.week} week`;
+            if (allTime.day) {
+                result = `${('0' + allTime.day).slice(-2)} day ${('0' + allTime.hour).slice(-2)} hh`;;
+            }
+            else if (allTime.hour) {
+                result = `${('0' + allTime.hour).slice(-2)} hh ${('0' + allTime.minute).slice(-2)} min`;
+            } else if (allTime.minute) {
+                result = `${('0' + allTime.minute).slice(-2)} min ${('0' + allTime.second).slice(-2)} sec`;
+            } else if (allTime.second) {
+                result = `${('0' + allTime.second).slice(-2)} sec`;
+            }
+            item.querySelectorAll('p')[1].innerText = `${result}'s ago`;
+        });
     }, 1000);
 }
-//day2
 
 function getTime(id) {
     let diffTime = Date.now() - id;
@@ -381,7 +410,7 @@ function getTime(id) {
         'second': parseInt(diffTime / 1000),
         'minute': parseInt(diffTime / (1000 * 60)),
         'hour': parseInt(diffTime / (1000 * 60 * 60)),
-        // 'day': parseInt(diffTime / (1000 * 60 * 60 * 24)),
+        'day': parseInt(diffTime / (1000 * 60 * 60 * 24)),
         // 'week': parseInt(diffTime / (1000 * 60 * 60 * 24 * 7)),
         // 'month': parseInt(diffTime / (1000 * 60 * 60 * 24 * 30)),
         // 'year': parseInt(diffTime / (1000 * 60 * 60 * 24 * 365)),
@@ -389,10 +418,9 @@ function getTime(id) {
     if (allTime.second > 60) allTime.second = allTime.second % 60;
     if (allTime.minute > 60) allTime.minute = allTime.minute % 60;
     if (allTime.hour > 60) allTime.hour = allTime.hour % 60;
-    // if (allTime.day > 60) allTime.day = allTime.day % 24;
+    if (allTime.day > 60) allTime.day = allTime.day % 24;
     // if (allTime.week > 7) allTime.week = allTime.week % 7;
     // if (allTime.month > 30) allTime.month = allTime.month % 30;
     // if (allTime.year > 365) allTime.year = allTime.year % 365;
-
     return allTime;
 }
